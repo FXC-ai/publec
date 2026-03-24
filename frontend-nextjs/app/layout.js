865dfa4c5435fda5ca873d5dd0a63c71c8957a7c
FXC-ai <fx.coindreau@gmail.com>
2026-03-24 17:19:29 +0100

    container postgres ajouté

---
+++ frontend-nextjs/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Laboratoire de FX Coindreau",
+  description: "Laboratoire de tests technologiques du C au Python en passant par Kubernetes",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
